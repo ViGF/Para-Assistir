0e8399751ee6fd54259b413f823b1d0e88d61332
Vitório Gomes <52535184+ViGF@users.noreply.github.com>
2022-10-19 17:16:17 -0300

    Atualizações

--- js/login.js
+++ js/login.js
@@ -1,4 +1,35 @@
-function login(form) {
-    console.log(form)
-    alert('login')
+function login(e) {
+    e.preventDefault()
+
+    const email = 'vitorio@gmail.com'
+    const password = 'senha123'
+
+    let email_informado = document.getElementById('username').value
+    let senha_informada = document.getElementById('password').value
+
+    if( email_informado = email && senha_informada == password) {
+        let continuar_logado = document.getElementById('remember-me').checked
+
+        if (continuar_logado) {
+            document.cookie = 'logado=1'
+        }
+
+        window.location.href = '/pages/main.html'
+    } else {
+        alert('Login ou senha inválidos!')
+    }
+}
+
+function register(e) {
+    e.preventDefault()
+
+    let username = document.getElementById('username').value
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    let passwordConfirm = document.getElementById('password-confirm').value
+
+    if (password !== passwordConfirm) {
+        alert('As senhas devem ser iguais!')
+    }
+
 }
